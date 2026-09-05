5b1c7a9d882e176d18e8c1892f0bc11ea0a828de
fatimarumaisa123 <139163529+fatimarumaisa123@users.noreply.github.com>
2026-09-05 04:45:33 -0700

    Delete task6.c

--- task6.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() 
-{
-    printf("Welcome to C programming \n\n\n\n");
-    printf("Name:\tAli\nAge:\t20\nGrade:\tA\n\n\n\n");
-    printf("File Path: C:\\Users\\Student\n\n\n\nHe said: \"Hello\"");
-    
-return 0;
-}
