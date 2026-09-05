70b4952ca785a69415a32da0a6ddc4f5806daa09
fatimarumaisa123 <139163529+fatimarumaisa123@users.noreply.github.com>
2026-09-05 04:09:47 -0700

    Add files via upload

---
+++ task6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    printf("Welcome to C programming \n\n\n\n");
+    printf("Name:\tAli\nAge:\t20\nGrade:\tA\n\n\n\n");
+    printf("File Path: C:\\Users\\Student\n\n\n\nHe said: \"Hello\"");
+    
+return 0;
+}
